27d81ec37f2bda55a46e4767a001c1e2075590f3
Butters <swki418@gmail.com>
2026-05-04 21:57:43 +0800

    init

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
