02b931b886332b3603b6fcd679215f43a9ded7db
Butters <swki418@gmail.com>
2026-05-05 17:48:21 +0800

    feat: implement Category and Product components with product fetching and display

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,4 +1,52 @@
+import { useEffect, useState } from "react";
+import { LuSlidersVertical } from "react-icons/lu";
+
+import type { ProductType } from "@/types/productType";
+import Product from "./components/Product";
+
 function Category() {
-  return <div>Category</div>;
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/api/productions.json");
+        const products = await response.json();
+        setProducts(products.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="grid grid-col-1 xl:grid-cols-4 xl:gap-5">
+      {/* filters */}
+      <aside className="col-span-1 border border-black/10 rounded-[20px] p-5 h-89.25 hidden xl:block">
+        <div className="flex justify-between items-center pb-6 border-b border-black/10">
+          <h2 className="text-xl font-bold">Filters</h2>
+          <button type="button" className="hover:bg-[#F0F0F0] p-2 rounded-full transition-colors duration-300">
+            <LuSlidersVertical className="w-5 h-5 text-black/40" />
+          </button>
+        </div>
+      </aside>
+
+      {/* 商品卡 */}
+      <section className="col-span-3">
+        <div className="flex justify-between items-end">
+          <h1 className="font-bold text-2xl xl:text-4xl">Casual</h1>
+          <p className="block xl:hidden text-sm text-black/60 pb-0.5">Showing 1-10 of 100 Products</p>
+          <button type="button" className="bg-[#F0F0F0] p-2 rounded-full block xl:hidden">
+            <LuSlidersVertical className="w-3.5 h-3.5 text-black" />
+          </button>
+        </div>
+
+        <ul className="grid grid-cols-2 md:grid-cols-3 gap-x-3.5 gap-y-6 md:gap-x-5 md:gap-y-9 mt-7 xl:mt-4">
+          {!!products && products.map((product) => <Product key={product.title} product={product} />)}
+        </ul>
+      </section>
+    </div>
+  );
 }
 export default Category;
--- src/pages/Category/components/Product.tsx
+++ src/pages/Category/components/Product.tsx
@@ -1,4 +1,51 @@
-function Product() {
-  return <div>Product</div>;
+import type { ProductType } from "@/types/productType";
+import { LiaStarSolid, LiaStarHalf } from "react-icons/lia";
+
+interface ProductProps {
+  product: ProductType;
+}
+
+function Product({ product }: ProductProps) {
+  const { title, price, score, cover, discount } = product;
+  const floorScore = Math.floor(score);
+  const hasHalfStar = score % 1 >= 0.5;
+  const discountedPrice = discount ? Math.floor(price * (1 - discount / 100)) : price; // discount 先視為折扣百分比
+  const marketPrice = discount ? price : null;
+
+  return (
+    <article>
+      <button type="button" className="group aspect-295/298 w-full overflow-hidden rounded-[14px] xl:rounded-[20px] relative">
+        <img src={cover} alt={title} className="h-full w-full object-cover hover:scale-105 transition-transform duration-300" />
+      </button>
+
+      {/* 品名 */}
+      <h3 className="mt-2.5 xl:mt-4 text-base xl:text-xl font-medium text-black line-clamp-1">{title}</h3>
+
+      {/* score */}
+      <div className="flex items-center mt-1 xl:mt-2 gap-1.5 xl:gap-3">
+        <ul className="flex items-center gap-1">
+          {Array.from({ length: floorScore }).map((_, index) => (
+            <li key={index}>
+              <LiaStarSolid className="text-[#FFC633] w-4 h-4 xl:w-4.5 xl:h-4.5" />
+            </li>
+          ))}
+          {hasHalfStar && (
+            <li>
+              <LiaStarHalf className="text-[#FFC633] w-4 h-4 xl:w-4.5 xl:h-4.5" />
+            </li>
+          )}
+        </ul>
+        <p className="text-sm xl:text-sm font-thin tracking-wide">
+          {score.toFixed(1)}/<span className="opacity-60">5</span>
+        </p>
+      </div>
+      {/* price */}
+      <div className="mt-0.5 xl:mt-2 flex items-center gap-1.5 xl:gap-2.5">
+        <p className="text-xl xl:text-2xl font-bold">${discountedPrice.toLocaleString()}</p>
+        {marketPrice && <p className="text-xl xl:text-2xl font-bold line-through text-black/40">${marketPrice.toLocaleString()}</p>}
+        {!!discount && <p className="text-[10px] xl:text-xs font-medium text-[#FF3333] py-1 px-2 xl:px-3 xl:py-1.5 bg-[#FF3333]/10 rounded-4xl">-{discount}%</p>}
+      </div>
+    </article>
+  );
 }
 export default Product;
