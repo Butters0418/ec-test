54729c6de015e04f5680b8323a78c1558198c14e
Butters <swki418@gmail.com>
2026-05-07 00:36:10 +0800

    feat: add decreaseQuantity and deleteItem methods to cart store

--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -4,7 +4,8 @@ import type { ProductType, CartItemType } from "@/types/productType";
 interface CartStore {
   cartItems: CartItemType[];
   addItem: (product: ProductType) => void;
-
+  decreaseQuantity: (title: string) => void;
+  deleteItem: (title: string) => void;
   clearCart: () => void;
 }
 
@@ -27,6 +28,24 @@ const useCartStore = create<CartStore>()(
             return { cartItems: [...state.cartItems, { ...product, quantity: 1 }] };
           }
         });
+      },
+
+      // 減少商品數量
+      decreaseQuantity: (title: string) => {
+        set((state) => ({
+          cartItems: state.cartItems
+            .map((item) => {
+              return item.title === title ? { ...item, quantity: item.quantity - 1 } : item;
+            })
+            .filter((item) => item.quantity > 0), // 移除數量為 0 的項目
+        }));
+      },
+
+      // 刪除單一 item
+      deleteItem: (title: string) => {
+        set((state) => ({
+          cartItems: state.cartItems.filter((item) => item.title !== title),
+        }));
       },
 
       // 清空購物車
